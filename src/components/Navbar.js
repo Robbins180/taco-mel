ede7913aab20455a66fb93f40effe5fa51146d70
Robbins180 <aRobbins180@gmail.com>
2021-08-29 09:09:11 -0500

    asdf

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+
+  function Navbar() {
+    return (
+      <>
+        <nav className='navbar'>
+          <div className='navbar-container'>
+            !-- Logo Image goes here --!
+            <a to='/' className='navbar-logo'>
+            </a>
+          </div>
+        </nav>
+      </>
+    )
+  }
+
+export default Navbar
