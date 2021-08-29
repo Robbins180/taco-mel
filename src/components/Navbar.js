64f4625901d7f2e16e094f218948d2f473cc2020
Robbins180 <aRobbins180@gmail.com>
2021-08-29 10:36:33 -0500

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './Navbar.css';
 
 
   function Navbar() {
@@ -10,8 +11,10 @@ import React, { useState, useEffect } from 'react';
         <nav className='navbar'>
 
           <div className='navbar-container'>
-            <a to='/' className='navbar-logo'>
+            <a href='/' to='/' className='navbar-logo'>
+            <img src='https://i.pinimg.com/originals/c3/0f/94/c30f94597b2c94ba91256413a1259266.jpg' alt='logo' width='80px' height='80px' />
             </a>
+            <h2 className='Mel'>Taco Mel</h2>
 
             <div className='menu-icon' onClick={handleClick}>
               <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
