e8310153a3e00464c4432da964b4d00bd3219b93
Robbins180 <aRobbins180@gmail.com>
2021-09-09 00:19:54 -0500

    broken hero slider

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,73 +1,57 @@
 import React, { useState } from 'react';
 import '../App.css';
 import './Hero.css';
-import { SliderData } from './SliderData';
-import { BiChevronLeft , BiChevronRight } from 'react-icons/bi';
+{/*  import { SliderData } from './SliderData';
+import { BiChevronLeft , BiChevronRight } from 'react-icons/bi'; */}
 
 
 
 
-const Hero = ({ slides }) => {
+const Hero = () => {
 
+ {/*
   const [current, setCurrent] = useState(0);
   const length = slides.length;
 
-  {/* Function  moves the next slide */}
-  const nextSlide = () => {
-      setCurrent(current === length - 1 ? 0 : current + 1);
+
+    const nextSlide = () => {
+      setCurrent(current === length - 1 ? 0 : current + 1)
+     };
+
+
+     const previousSlide = () => {
+     setCurrent(current === 0 ? length - 1 : current - 1)
     };
 
 
-  const previousSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
+    if (!Array.isArray(slides) || slides.length <= 0) {
+      return null;
+     }
 
-
-  if(!Array.isArray(slides) || slides.length <= 0) {
-    return null;
-  }
+   */ }
 
   return (
-    <div className='hero-container'>
+      <div className='hero-container'>
 
-      <div className='slider'>
+        <img src='./images/TB-interior.jpeg' className='hero-img' alt='nothing' />
+        <h1>¡Yo quiero Taco Bell!</h1>
+        <p>Now Serving the Mexican Pizza</p>
+          <div className='hero-btns'>
 
-        <BiChevronLeft className='left-arrow' onClick={previousSlide} />
-        <BiChevronRight className='right-arrow' onClick={nextSlide} />
-        {SliderData.map((slide, index) => {
-          return (
-
-            <div
-            className={index === current ? 'slide active' : 'slide'} key={index}>
-
-              {index === current &&
-                (<img src={slide.image} className='hero-img' alt='nothing' />
-              )}
-
-            </div>
-          );
-        })};
+            <a href='/Menu'>
+              <button className='btns'>
+            GET STARTED
+              </button>
+            </a>
+            <a href='#'>
+              <button
+                className='btns'
+                onClick={console.log('hey')}>
+                BEGIN NOSTALGIA <i className='far fa-play-circle' />
+              </button>
+            </a>
+          </div>
       </div>
-
-
-      <h1>¡Yo quiero Taco Bell!</h1>
-      <p>Now Serving the Mexican Pizza</p>
-      <div className='hero-btns'>
-
-        <a href='/Menu'>
-          <button className='btns'>
-        GET STARTED
-          </button>
-        </a>
-        <a href='#'>
-          <button
-            className='btns'
-            onClick={console.log('hey')}>
-            BEGIN NOSTALGIA <i className='far fa-play-circle' />
-          </button>
-        </a>
-      </div>
-    </div>
   )
 };
 
