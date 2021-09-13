226049c17c1669875dc5242375125a4c6bb57a13
Robbins180 <aRobbins180@gmail.com>
2021-09-13 02:39:57 -0500

    footerInfo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,25 @@ import './Footer.css'
 
 function Footer() {
   return (
-    
+    <>
+      <div className='footer'>
+        <div className='footer-container'>
+          <div className='first-half-box'>
+              <p>Deals</p>
+              <p>About Us</p>
+              <p>Legal</p>
+              <p>FAQ</p>           
+              <p>Careers</p>
+              <p>Conact Us</p>
+          </div>
+          <div className='second-half-box'>
+            <p>At participating locations. Prices and items may vary. Tax extra. 2,000 calories a day used for general nutrition advice, but calorie needs vary. Additional nutrition information available upon request.</p>
+            <img src='#'></img>
+            <p>2021 Taco Mel by Drew Robbins</p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
